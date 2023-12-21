f01b785d7556718be971eef7f23604498df0fb3b
Deepika-EC <deepikaramulu0104@gmail.com>
2023-12-21 12:33:10 +0530

    studentform
    student

--- src/Login.js
+++ src/Login.js
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { initializeApp } from "firebase/app";
 import { getAuth,signInWithEmailAndPassword } from "firebase/auth";
